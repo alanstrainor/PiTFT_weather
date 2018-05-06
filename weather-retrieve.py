e3af64de3584371f62f84dc442a8ea698980a041
Alan <alanstrainor@gmail.com>
2018-05-06 16:06:39 +0100

    Working forecast and formatting

--- weather-retrieve.py
+++ weather-retrieve.py
@@ -39,10 +39,20 @@ class Weather():
 		player = avg.Player.get()
 		canvas = player.createMainCanvas(size=(480,320))
 		rootNode = canvas.getRootNode()
-		avg.WordsNode(pos=(10,10), font="arial",text="Wind Speed: " + windSpeed, parent=rootNode)
-		avg.WordsNode(pos=(10,30), font="arial",text="Temp: " + currTemp, parent=rootNode)
-		imgNode = avg.ImageNode(href="images/" + imageCode + ".png", pos=(10,30),parent=player.getRootNode())
+		avg.WordsNode(pos=(10,10), font="arial",text="Currently: " + station, parent=rootNode)
+		avg.WordsNode(pos=(10,30), font="arial",text="Wind Speed: " + windSpeed, parent=rootNode)
+		avg.WordsNode(pos=(10,50), font="arial",text="Temp: " + currTemp, parent=rootNode)
+		avg.WordsNode(pos=(10,70), font="arial",text="Wind Direction: " + windDir, parent=rootNode)
+		imgNode = avg.ImageNode(href="images/" + imageCode + ".png", pos=(10,70),parent=player.getRootNode())
 
+		# Forecast for coming days
+		column = 10
+		for i in range(2,6):
+			avg.WordsNode(pos=(column,200), font="arial",text=forecastDays[i], parent=rootNode)
+			avg.WordsNode(pos=(column,220), font="arial",text=forecastText[i], parent=rootNode)
+			avg.WordsNode(pos=(column,240), font="arial",text=forecastHighs[i] + "c", parent=rootNode)
+			avg.WordsNode(pos=(column,260), font="arial",text=forecastLows[i] + "c", parent=rootNode)
+			column += 130
 		player.play()
 
 
@@ -58,21 +68,23 @@ while(is_run):
 	# Get weather forecasts for the upcoming days.
 
 	forecasts = location.forecast
-	#station = forecast.text
+	station = location.condition.text
 	today = str(now)
 	windSpeed = location.wind.speed
+	windDir = location.wind.direction
 	currTemp = location.condition.temp
 	currPress = location.atmosphere['pressure']
 	imageCode = location.condition.code
 
 	forecasts = location.forecast
 	for forecast in forecasts:
+	#for i in range(5):
 		forecastCounter += 1
 		forecastText[forecastCounter] = forecast.text
 		forecastDays[forecastCounter] = forecast.day
 		forecastHighs[forecastCounter] = forecast.high
 		forecastLows[forecastCounter] = forecast.low
-
+	print(forecastText)
 	# extract current data for today
     #today = weather['forecasts'][0]['day_of_week'][0:3] + " " \
     #      + weather['forecasts'][0]['date'][4:] + " " \
